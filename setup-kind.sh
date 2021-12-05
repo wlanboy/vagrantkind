1f26c6b6cc3b954f9a67cbdbc4a8979c24945311
wlanboy <me@wlanboy.com>
2021-12-05 12:24:13 +0000

    added kind setup with calico and istio

--- setup-kind.sh
+++ setup-kind.sh
@@ -9,9 +9,9 @@ kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 kubectl scale deployment --replicas 1 coredns --namespace kube-system
 
 # Istio
-#./istioctl manifest apply --set profile=default
-#./istioctl manifest generate --set profile=default | ./istioctl verify-install -f -
-#kubectl patch service istio-ingressgateway -n istio-system --patch "$(cat istio-settings.yaml)"
+./istioctl manifest apply --set profile=default
+./istioctl manifest generate --set profile=default | ./istioctl verify-install -f -
+kubectl patch service istio-ingressgateway -n istio-system --patch "$(cat istio-settings.yaml)"
 
 # Delete cluster
 #kind delete clusters kindcluster
