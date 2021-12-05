adeadbb718f09704a63cdf4ebd2338811e0addba
wlanboy <me@wlanboy.com>
2021-12-05 12:21:16 +0000

    added package install for amd64 and arm64

---
+++ arm64-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+
+sudo apt-get install -y nano htop apt-transport-https ca-certificates git curl gnupg2 software-properties-common lsb-release wget net-tools nfs-common
+#sudo apt install openjdk-11-jdk-headless maven
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io kubectl
