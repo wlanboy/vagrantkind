c3881e7d0993e0901a5c0a6a3423eae76c3d05f4
wlanboy <wlanboy@users.noreply.github.com>
2021-12-15 16:58:22 +0100

    Create arm64-armbian-packages.sh

---
+++ arm64-armbian-packages.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https software-properties-common net-tools
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
