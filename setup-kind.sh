800cafd51c8ccfada17751a5abb08cc94b5a57c7
wlanboy <wlanboy@users.noreply.github.com>
2021-12-27 17:09:16 +0100

    Update setup-kind.sh

--- setup-kind.sh
+++ setup-kind.sh
@@ -8,6 +8,9 @@ kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 # CoreDNS
 kubectl scale deployment --replicas 1 coredns --namespace kube-system
 
+# Ingress
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
 # Istio
 ./istioctl manifest apply --set profile=default
 ./istioctl manifest generate --set profile=default | ./istioctl verify-install -f -
