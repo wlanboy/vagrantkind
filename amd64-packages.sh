af135918f24d0c625fed74baf9194bb23b5ded7f
wlanboy <me@wlanboy.com>
2022-02-05 05:27:20 +0000

    updated packages

--- amd64-packages.sh
+++ amd64-packages.sh
@@ -7,6 +7,6 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 
-sudo apt-get install -y nano htop apt-transport-https ca-certificates curl gnupg2 software-properties-common lsb-release wget net-tools
+sudo apt-get install -y nano htop apt-transport-https ca-certificates curl gnupg2 software-properties-common lsb-release wget net-tools git
 #sudo apt install openjdk-11-jdk-headless maven
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
