b08c59bf7fb148b7841678c05bc0ed25a4ad02c6
wlanboy <me@wlanboy.com>
2022-10-22 07:44:06 +0200

    updated versions of tools

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -6,14 +6,14 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 sudo cp ./kubectl /usr/bin
 
-wget https://get.helm.sh/helm-v3.7.1-linux-arm64.tar.gz
-tar -zxvf helm-v3.7.1-linux-arm64.tar.gz
+wget https://get.helm.sh/helm-v3.10.1-linux-arm64.tar.gz
+tar -zxvf helm-v3.10.1-linux-arm64.tar.gz
 sudo cp linux-arm64/helm /usr/local/bin/helm
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-arm64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.16.1/kind-linux-arm64
 chmod +x ./kind
 sudo cp ./kind /usr/bin
 
-wget https://github.com/istio/istio/releases/download/1.11.5/istio-1.11.5-linux-arm64.tar.gz
-tar -zxvf istio-1.11.5-linux-arm64.tar.gz
-cp istio-1.11.5/bin/istioctl ~
+wget https://github.com/istio/istio/releases/download/1.15.2/istio-1.15.2-linux-arm64.tar.gz
+tar -zxvf istio-1.15.2-linux-arm64.tar.gz
+cp istio-1.15.2/bin/istioctl ~
