5d88a487b2c63f584645515a9d78fc440b3174d4
wlanboy <wlanboy@users.noreply.github.com>
2022-12-31 10:08:09 +0100

    Update setup-kind.sh

--- setup-kind.sh
+++ setup-kind.sh
@@ -8,12 +8,12 @@ kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 # CoreDNS
 kubectl scale deployment --replicas 1 coredns --namespace kube-system
 
-# Ingress
+# Ingress as an alternative to istio
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 # Istio
 ./istioctl manifest apply --set profile=default
-./istioctl manifest generate --set profile=default | ./istioctl verify-install -f --skip-confirmation -
+#./istioctl manifest generate --set profile=default | ./istioctl verify-install -f --skip-confirmation -
 kubectl patch service istio-ingressgateway -n istio-system --patch "$(cat istio-settings.yaml)"
 
 # Delete cluster
