44e0544e98eff283f3731a701b4f4ef177ee850c
wlanboy <wlanboy@users.noreply.github.com>
2023-04-15 09:23:01 +0200

    new helm version

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -6,8 +6,8 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 sudo cp ./kubectl /usr/bin
 
-wget https://get.helm.sh/helm-v3.10.1-linux-arm64.tar.gz
-tar -zxvf helm-v3.10.1-linux-arm64.tar.gz
+wget https://get.helm.sh/helm-v3.11.3-linux-arm64.tar.gz
+tar -zxvf helm-v3.11.3-linux-arm64.tar.gz
 sudo cp linux-arm64/helm /usr/local/bin/helm
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-arm64
