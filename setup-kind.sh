0cd6991569a2aaee9a23f7125b7a287aacca9579
wlanboy <wlanboy@users.noreply.github.com>
2023-05-07 16:21:59 +0200

    updated calico operator

--- setup-kind.sh
+++ setup-kind.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 kind create cluster --config=kind-config.yaml
-# Calico
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
-kubectl apply -f calico.yaml
+
+# Calico operator
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml -O
+kubectl create -f tigera-operator.yaml
+
+# Calico cr
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml -O
+kubectl create -f custom-resources.yaml
+
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 # CoreDNS
 kubectl scale deployment --replicas 1 coredns --namespace kube-system
 
 # Ingress as an alternative to istio
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+# curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml -O
+# kubectl apply -f deploy.yaml
 
 # Istio
 ./istioctl manifest apply --set profile=default
