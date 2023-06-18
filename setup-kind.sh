cf8712b1914feb5123015181b300e84a4ca24232
wlanboy <wlanboy@users.noreply.github.com>
2023-06-18 08:42:36 +0200

    Added Istio Helm and Metallb

--- setup-kind.sh
+++ setup-kind.sh
@@ -18,10 +18,26 @@ kubectl scale deployment --replicas 1 coredns --namespace kube-system
 # curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml -O
 # kubectl apply -f deploy.yaml
 
+# metallb
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+#kubectl apply -f metallb-pool.yaml
+#kubectl apply -f metallb-adv.yaml
+
 # Istio
-./istioctl manifest apply --set profile=default
+#./istioctl manifest apply --set profile=default
 #./istioctl manifest generate --set profile=default | ./istioctl verify-install -f --skip-confirmation -
-kubectl patch service istio-ingressgateway -n istio-system --patch "$(cat istio-settings.yaml)"
+#kubectl patch service istio-ingressgateway -n istio-system --patch "$(cat istio-settings.yaml)"
+
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo update
+kubectl create namespace istio-system
+
+helm install istio-base istio/base -n istio-system
+helm install istiod istio/istiod -n istio-system --wait
+
+kubectl create namespace istio-ingress
+helm install istio-ingress istio/gateway -n istio-ingress --wait
+helm install istio-ingressgateway istio/gateway -n istio-ingress
 
 # Delete cluster
 #kind delete clusters kindcluster
