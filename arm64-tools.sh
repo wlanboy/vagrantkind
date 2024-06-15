8f9c4b43eaac3f3e26886eab1e88bfcb95bcd660
wlanboy <wlanboy@users.noreply.github.com>
2024-06-15 18:13:04 +0200

    Update arm64-tools.sh

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -4,7 +4,7 @@ cd ~
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 chmod +x ./kubectl
-sudo cp ./kubectl /usr/bin
+sudo cp ./kubectl /usr/local/bin
 
 wget https://get.helm.sh/helm-v3.11.3-linux-arm64.tar.gz
 tar -zxvf helm-v3.11.3-linux-arm64.tar.gz
@@ -12,8 +12,8 @@ sudo cp linux-arm64/helm /usr/local/bin/helm
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-arm64
 chmod +x ./kind
-sudo cp ./kind /usr/bin
+sudo cp ./kind /usr/local/bin
 
 wget https://github.com/istio/istio/releases/download/1.17.2/istio-1.17.2-linux-arm64.tar.gz
 tar -zxvf istio-1.17.2-linux-arm64.tar.gz
-cp istio-1.17.2/bin/istioctl ~
+sudo cp istio-1.17.2/bin/istioctl /usr/local/bin
