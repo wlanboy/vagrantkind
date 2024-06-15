1e1492f9f7659a5075c6e70bf53b528f74aeb280
wlanboy <wlanboy@users.noreply.github.com>
2024-06-15 18:12:41 +0200

    Update amd64-tools.sh

--- amd64-tools.sh
+++ amd64-tools.sh
@@ -4,7 +4,7 @@ cd ~
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
-sudo cp ./kubectl /usr/bin
+sudo cp ./kubectl /usr/local/bin
 
 cd /home/vagrant && wget https://get.helm.sh/helm-v3.11.3-linux-amd64.tar.gz
 tar -zxvf helm-v3.11.3-linux-amd64.tar.gz
@@ -16,4 +16,4 @@ sudo mv ./kind /usr/bin
 
 wget https://github.com/istio/istio/releases/download/1.17.2/istio-1.17.2-linux-amd64.tar.gz
 tar -zxvf istio-1.17.2-linux-amd64.tar.gz
-cp istio-1.17.2/bin/istioctl ~
+sudo cp istio-1.17.2/bin/istioctl /usr/local/bin
