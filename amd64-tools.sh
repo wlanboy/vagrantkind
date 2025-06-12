01001f20c452e5fa4e00036729961442f87f9c3f
wlanboy <wlanboy@users.noreply.github.com>
2025-06-12 18:10:42 +0200

    Update amd64-tools.sh versions vars

--- amd64-tools.sh
+++ amd64-tools.sh
@@ -6,14 +6,18 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 sudo cp ./kubectl /usr/local/bin
 
-cd /home/vagrant && wget https://get.helm.sh/helm-v3.18.2-linux-amd64.tar.gz
-tar -zxvf helm-v3.18.2-linux-amd64.tar.gz
+HELM_VERSION="3.18.2"
+KIND_VERSION="0.29.0"
+ISTIO_VERSION="1.26.1"
+
+cd /home/vagrant && wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+tar -zxvf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 sudo cp linux-amd64/helm /usr/local/bin/helm
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v${KIND_VERSION}/kind-linux-amd64"
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin
 
-wget https://github.com/istio/istio/releases/download/1.26.1/istio-1.26.1-linux-amd64.tar.gz
-tar -zxvf istio-1.26.1-linux-amd64.tar.gz
-sudo cp istio-1.26.1/bin/istioctl /usr/local/bin
+wget "https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-amd64.tar.gz"
+tar -zxvf "istio-${ISTIO_VERSION}-linux-amd64.tar.gz"
+sudo cp "istio-${ISTIO_VERSION}/bin/istioctl" /usr/local/bin
