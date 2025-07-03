876f8c926cf9a5e0096be69e852d75f9a3447fb6
wlanboy <wlanboy@users.noreply.github.com>
2025-07-03 11:40:30 +0200

    Update loki.sh

--- mesh/loki.sh
+++ mesh/loki.sh
@@ -3,3 +3,5 @@ helm repo update
 
 kubectl create ns loki
 helm upgrade --install loki grafana/loki --namespace loki
+
+helm upgrade --install promtail grafana/promtail --namespace loki -f promtail-values.yaml
