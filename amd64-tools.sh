35164c6f08a6ec88b3995fdd26b131c339a381ce
wlanboy <wlanboy@users.noreply.github.com>
2025-07-05 08:32:28 +0200

    Update amd64-tools.sh new istio release

--- amd64-tools.sh
+++ amd64-tools.sh
@@ -8,7 +8,7 @@ sudo cp ./kubectl /usr/local/bin
 
 HELM_VERSION="3.18.2"
 KIND_VERSION="0.29.0"
-ISTIO_VERSION="1.26.1"
+ISTIO_VERSION="1.26.2"
 
 cd /home/vagrant && wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 tar -zxvf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
