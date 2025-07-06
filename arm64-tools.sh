8a4a3bf6321106f1d8587c66e068fcad4521fa36
wlanboy <wlanboy@users.noreply.github.com>
2025-07-06 11:57:39 +0200

    Update arm64-tools.sh added k9s

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -9,6 +9,7 @@ sudo cp ./kubectl /usr/local/bin
 HELM_VERSION="3.18.2"
 KIND_VERSION="0.29.0"
 ISTIO_VERSION="1.26.1"
+K9S_VERSION="0.50.7"
 
 wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "helm-v${HELM_VERSION}-linux-arm64.tar.gz"
@@ -21,3 +22,7 @@ sudo cp ./kind /usr/local/bin
 wget "https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "istio-${ISTIO_VERSION}-linux-arm64.tar.gz"
 sudo cp "istio-${ISTIO_VERSION}/bin/istioctl" /usr/local/bin
+
+wget "https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_amd64.tar.gz"
+tar -zxvf k9s_Linux_amd64.tar.gz
+sudo cp ./k9s /usr/local/bin
