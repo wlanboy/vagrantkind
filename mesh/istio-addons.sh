3e75c74e350e20546421c7377b6805a5ada51f46
wlanboy <wlanboy@github.com>
2025-07-12 07:45:08 +0200

    Patch Loki url for graphana datasources

--- mesh/istio-addons.sh
+++ mesh/istio-addons.sh
@@ -13,7 +13,6 @@ kubectl apply -f ${OBS_FOLDER}/gateway-monitoring.yaml
 kubectl apply -f ${OBS_FOLDER}/virtual-service-monitoring.yaml
 
 kubectl patch deployment grafana -n istio-system --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/env", "value": [{"name": "GF_SERVER_ROOT_URL", "value": "http://monitoring.ser.local/grafana/"}, {"name": "GF_SERVER_SERVE_FROM_SUB_PATH", "value": "true"}]}]'
-kubectl rollout restart deployment/grafana -n istio-system
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
@@ -23,3 +22,7 @@ helm upgrade --install --values ${OBS_FOLDER}/promtail-overrides.yaml promtail g
 
 kubectl apply -f ${OBS_FOLDER}/istio-telemetry-envoy.yaml 
 #kubectl patch istiooperator <your-istiooperator-name> -n istio-system --type='json' -p='[{"op": "add", "path": "/spec/meshConfig/accessLogFile", "value": "/dev/stdout"}]'
+
+kubectl get configmap grafana -n istio-system -o yaml > grafana-configmap.yaml
+sed -i 's|http://loki:3100|http://loki-loki-distributed-gateway.grafana-loki.svc.cluster.local|g' grafana-configmap.yaml
+kubectl apply -f grafana-configmap.yaml
