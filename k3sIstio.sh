2165daa62d45914303a435ad5a7f5a38355c605d
wlanboy <wlanboy@users.noreply.github.com>
2025-11-26 14:59:23 +0100

    Create k3sIstio.sh

---
+++ k3sIstio.sh
@@ -0,0 +1,10 @@
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_CHANNEL=stable INSTALL_K3S_EXEC="--disable=traefik --disable=servicelb" sh -
+mkdir ~/.kube
+sudo cp -i /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chown $USER:$USER ~/.kube/config
+
+istioctl install --set profile=default -y
+
+kubectl patch svc istio-ingressgateway -n istio-system \
+  -p '{"spec": {"type": "NodePort", "ports":[{"name":"http2","port":80,"nodePort":30080},{"name":"https","port":443,"nodePort":30443}]}}'
+kubectl get svc istio-ingressgateway -n istio-system
