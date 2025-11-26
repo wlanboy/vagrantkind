e4e8c70dc0b71c7a4c54e6cdec2bcb2e527c1d60
wlanboy <wlanboy@users.noreply.github.com>
2025-11-26 14:38:51 +0100

    Update amd64-packages.sh

--- amd64-packages.sh
+++ amd64-packages.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-nobel main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 
 sudo apt-get install -y nano htop apt-transport-https ca-certificates curl gnupg2 software-properties-common lsb-release wget net-tools git
