3950d661525610d0dac4999bfdb95bb177ab55c8
wlanboy <wlanboy@users.noreply.github.com>
2025-11-26 14:37:28 +0100

    Update arm64-tools.sh

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -6,10 +6,14 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 sudo cp ./kubectl /usr/local/bin
 
-HELM_VERSION="3.18.2"
-KIND_VERSION="0.29.0"
-ISTIO_VERSION="1.26.1"
-K9S_VERSION="0.50.7"
+# see https://github.com/helm/helm/releases
+HELM_VERSION="3.19.2"
+# see https://github.com/kubernetes-sigs/kind/releases/
+KIND_VERSION="0.30.0"
+# see https://github.com/istio/istio/releases/
+ISTIO_VERSION="1.28.0"
+# see https://github.com/derailed/k9s/releases
+K9S_VERSION="0.50.16"
 
 wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "helm-v${HELM_VERSION}-linux-arm64.tar.gz"
