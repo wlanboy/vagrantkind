2982961f5d903144dfa99e5049233d12dc9de079
wlanboy <wlanboy@users.noreply.github.com>
2025-12-12 21:49:07 +0100

    Update run.sh

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,5 +2,5 @@ docker run -d \
   --name nginx-ssl-proxy \
   -p 443:443 \
   -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
-  -v $(pwd)/certs:/certs:ro \
+  -v /certs:/etc/nginx//certs:ro \
   nginx:latest
