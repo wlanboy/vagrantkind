04e4293f30f55753c250414d2c38c14d79dcee11
wlanboy <wlanboy@github.com>
2025-12-13 07:16:43 +0100

    added kubeconfig management

--- install-local-k3s.sh
+++ install-local-k3s.sh
@@ -8,6 +8,10 @@ echo "IP: $LOKALE_IP"
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_CHANNEL=stable INSTALL_K3S_EXEC="--disable=traefik --node-external-ip=$LOKALE_IP" sh -
 
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/k3s.yaml
+sudo chown $USER:$USER ~/.kube/k3s.yaml
+sed -i "s/127.0.0.1/$LOKALE_IP/" ~/.kube/k3s.yaml
+
 echo "Installing MetalLB (version ${METALLB_VERSION})..."
 METALLB_MANIFEST_URL="https://raw.githubusercontent.com/metallb/metallb/v${METALLB_VERSION}/config/manifests/metallb-native.yaml"
 kubectl apply -f "${METALLB_MANIFEST_URL}"
