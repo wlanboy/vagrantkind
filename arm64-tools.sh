daf718f224fbf73ac5a3bf0b2aa83a5251e8c9d1
wlanboy <wlanboy@users.noreply.github.com>
2026-01-06 10:00:16 +0100

    removed argocd

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -12,10 +12,6 @@ HELM_VERSION="3.19.4"
 KIND_VERSION="0.31.0"
 # see https://github.com/istio/istio/releases/
 ISTIO_VERSION="1.28.2"
-# see https://github.com/derailed/k9s/releases
-K9S_VERSION="0.50.16"
-#see https://github.com/argoproj/argo-cd/releases
-ARGOCD_VERSION="v3.2.3"
 
 wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "helm-v${HELM_VERSION}-linux-arm64.tar.gz"
@@ -28,11 +24,3 @@ sudo cp ./kind /usr/local/bin
 wget "https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "istio-${ISTIO_VERSION}-linux-arm64.tar.gz"
 sudo cp "istio-${ISTIO_VERSION}/bin/istioctl" /usr/local/bin
-
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/${ARGOCD_VERSION}/argocd-linux-arm64
-sudo install -m 555 argocd-linux-arm64 /usr/local/bin/argocd
-rm argocd-linux-arm64
-
-wget "https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_arm64.tar.gz"
-tar -zxvf k9s_Linux_arm64.tar.gz
-sudo cp ./k9s /usr/local/bin
