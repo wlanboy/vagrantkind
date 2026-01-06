d31cfcbb0ac8fc4c069be8360d115ec0a71580cf
wlanboy <wlanboy@users.noreply.github.com>
2026-01-06 09:55:24 +0100

    Update arm64-tools.sh - minor versions, new tools

--- arm64-tools.sh
+++ arm64-tools.sh
@@ -7,13 +7,15 @@ chmod +x ./kubectl
 sudo cp ./kubectl /usr/local/bin
 
 # see https://github.com/helm/helm/releases
-HELM_VERSION="3.19.2"
+HELM_VERSION="3.19.4"
 # see https://github.com/kubernetes-sigs/kind/releases/
-KIND_VERSION="0.30.0"
+KIND_VERSION="0.31.0"
 # see https://github.com/istio/istio/releases/
-ISTIO_VERSION="1.28.0"
+ISTIO_VERSION="1.28.2"
 # see https://github.com/derailed/k9s/releases
 K9S_VERSION="0.50.16"
+#see https://github.com/argoproj/argo-cd/releases
+ARGOCD_VERSION="v3.2.3"
 
 wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "helm-v${HELM_VERSION}-linux-arm64.tar.gz"
@@ -26,6 +28,14 @@ sudo cp ./kind /usr/local/bin
 wget "https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-arm64.tar.gz"
 tar -zxvf "istio-${ISTIO_VERSION}-linux-arm64.tar.gz"
 sudo cp "istio-${ISTIO_VERSION}/bin/istioctl" /usr/local/bin
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/${ARGOCD_VERSION}/argocd-linux-arm64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+wget https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_arm64
+sudo install -m 555 hey_linux_amd64 /usr/local/bin/hey
+rm hey_linux_amd64
 
 wget "https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_amd64.tar.gz"
 tar -zxvf k9s_Linux_amd64.tar.gz
