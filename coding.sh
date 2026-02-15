c38b2b77954fa9c72e58059fe501cf05d2c839f5
wlanboy <wlanboy@users.noreply.github.com>
2026-02-15 17:52:54 +0100

    Update sdkman commands and install additional packages
    Updated sdkman installation commands to include Maven and added system package installations.

--- coding.sh
+++ coding.sh
@@ -9,10 +9,10 @@ curl -s "https://get.sdkman.io" | bash
 # Quellcode für sdkman aktivieren (nötig, um sdkman Befehle zu verwenden)
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# Aktualisiere sdkman und installiere Java 25
+# Aktualisiere sdkman und installiere Java 25 und maven
 sdk update
 sdk install java 25-tem
-
-# Aktualisiere sdkman und installiere Maven 3.9.9
-sdk update
 sdk install maven 3.9.9
+
+sudo apt-get update
+sudo apt-get install net-tools git nano htop 
