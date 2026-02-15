3a3d3909c148f03f2859cd3dec9fadbd32571462
wlanboy <wlanboy@users.noreply.github.com>
2026-02-15 18:53:12 +0100

    Fix Exec command formatting in desktop entries
    Updated execution command for MarkText and LM Studio desktop entries.

--- coding.sh
+++ coding.sh
@@ -40,7 +40,7 @@ touch "$DESKTOP_FILE"
 cat > "$DESKTOP_FILE" <<EOF
 [Desktop Entry]
 Name=MarkText
-Exec="$APPIMAGE_PATH" --no-sandbox
+Exec=$APPIMAGE_PATH --no-sandbox
 Icon=marktext
 Type=Application
 Categories=Utility;TextEditor;
@@ -48,6 +48,8 @@ Terminal=false
 EOF
 
 chmod +x $DESKTOP_FILE
+gio set $DESKTOP_FILE metadata::trusted true
+
 echo "Desktop-Datei erstellt unter: $DESKTOP_FILE"
 
 wget "https://github.com/marktext/marktext/blob/develop/resources/icons/icon.png?raw=true" \
@@ -68,7 +70,7 @@ touch "$DESKTOP_FILE"
 cat > "$DESKTOP_FILE" <<EOF
 [Desktop Entry]
 Name=LM Studio
-Exec="$APPIMAGE_PATH" --no-sandbox
+Exec=$APPIMAGE_PATH --no-sandbox
 Icon=lmstudio
 Type=Application
 Categories=Utility;Development;
@@ -76,6 +78,8 @@ Terminal=false
 EOF
 
 chmod +x $DESKTOP_FILE
+gio set $DESKTOP_FILE metadata::trusted true
+
 echo "Desktop-Datei erstellt unter: $DESKTOP_FILE"
 
 wget "https://lmstudio.ai/assets/android-chrome-512x512.png" \
