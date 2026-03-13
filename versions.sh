0e1954ea7ef2e073780650052ebe26ba8b07c3e4
wlanboy <wlanboy@github.com>
2026-03-13 17:58:45 +0100

    Helm (helm/helm):   HELM_VERSION 3.20 -> 4.1.3 Kind (kubernetes-sigs/kind):   KIND_VERSION ist aktuell: 0.31.0 Istio (istio/istio):   ISTIO_VERSION 1.28.3 -> 1.29.1 K9s (derailed/k9s):   K9S_VERSION ist aktuell: 0.50.18 ArgoCD (argoproj/argo-cd):   ARGOCD_VERSION v3.3.0 -> v3.3.3

--- versions.sh
+++ versions.sh
@@ -3,19 +3,19 @@
 # Diese Datei wird von amd64-tools.sh und arm64-tools.sh eingebunden
 
 # https://github.com/helm/helm/releases
-HELM_VERSION="3.20"
+HELM_VERSION="4.1.3"
 
 # https://github.com/kubernetes-sigs/kind/releases/
 KIND_VERSION="0.31.0"
 
 # https://github.com/istio/istio/releases/
-ISTIO_VERSION="1.28.3"
+ISTIO_VERSION="1.29.1"
 
 # https://github.com/derailed/k9s/releases
 K9S_VERSION="0.50.18"
 
 # https://github.com/argoproj/argo-cd/releases
-ARGOCD_VERSION="v3.3.0"
+ARGOCD_VERSION="v3.3.3"
 
 # https://sdkman.io/ -> sdk list java / sdk list maven
 JAVA_VERSION="25-tem"
