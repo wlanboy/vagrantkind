5564254274f4306dcb5ad5de2e842915b7ed72d0
wlanboy <wlanboy@github.com>
2026-03-21 17:17:13 +0100

    ARGOCD_VERSION aktualisiert: v3.3.3 -> v3.3.4

--- versions.sh
+++ versions.sh
@@ -15,7 +15,7 @@ ISTIO_VERSION="1.29.1"
 K9S_VERSION="0.50.18"
 
 # https://github.com/argoproj/argo-cd/releases
-ARGOCD_VERSION="v3.3.3"
+ARGOCD_VERSION="v3.3.4"
 
 # https://sdkman.io/ -> sdk list java / sdk list maven
 JAVA_VERSION="25-tem"
