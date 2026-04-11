ef0c1d90789b0afe2e453dd98f642c9b2beee3f9
wlanboy <wlanboy@github.com>
2026-04-11 09:41:38 +0200

    use version.sh

--- install-wsl-kind.sh
+++ install-wsl-kind.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-METALLB_VERSION="0.15.2"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Prüfen ob benötigte Tools vorhanden sind
@@ -23,17 +22,6 @@ kind create cluster --config="$SCRIPT_DIR/kind-local.yaml"
 
 echo "Warte auf Cluster..."
 kubectl wait --for=condition=Ready nodes --all --timeout=120s
-
-echo "Installing MetalLB (version ${METALLB_VERSION})..."
-METALLB_MANIFEST_URL="https://raw.githubusercontent.com/metallb/metallb/v${METALLB_VERSION}/config/manifests/metallb-native.yaml"
-kubectl apply -f "${METALLB_MANIFEST_URL}"
-
-echo "Warte auf MetalLB Pods..."
-kubectl -n metallb-system wait --for=condition=Ready --all pods --timeout=120s
-
-kubectl apply -f "$SCRIPT_DIR/wsl-metallb-pool.yaml"
-kubectl apply -f "$SCRIPT_DIR/wsl-metallb-adv.yaml"
-echo "MetalLB konfiguriert."
 
 echo "Kind Cluster Installation abgeschlossen."
 
